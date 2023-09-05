baa801760ecb519734b2934a6492edaee3611775
Juan Carlos Largo <134282467+imlargo@users.noreply.github.com>
2023-09-05 19:51:02 +0000

    Creacion de clases

---
+++ Proyecto Final/src/Asiento.java
@@ -0,0 +1,18 @@
+import Pasajero;
+
+public class Asiento {
+    private String tipo;
+    private Pasajero pasajero;
+    private int n_asiento;
+    private Boleto Boleto
+
+    public Asiento(String tipo, Pasajero pasajero, int n_asiento, ) {
+        self.tipo = tipo;
+        self.pasajero = pasajero;
+        self.n_asiento = n_asiento;
+    }
+
+    
+
+
+}
