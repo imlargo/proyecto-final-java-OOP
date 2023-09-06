78714bb8aba2f9599cc796742ea28023e88a3467
Juan Carlos Largo <134282467+imlargo@users.noreply.github.com>
2023-09-06 11:19:55 -0500

    Update App.java

--- Proyecto Final/src/App.java
+++ Proyecto Final/src/App.java
@@ -1,5 +1,58 @@
+import java.util.Scanner;
+
+
 public class App {
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+
+        System.out.println("Bienvenido")
+
+        //haganme los mensajes para q diga las opciones disponibles
+        Scanner scanner = new Scanner(System.in);
+        int opcion;
+
+        do {
+            System.out.println("Menú:");
+            System.out.println("1. Comprar vuelo");
+            System.out.println("2. Reasignar vuelo");
+            System.out.println("3. Cancelar vuelo");
+            System.out.println("4. Ver cuenta");
+            System.out.println("5. Salir");
+            System.out.print("Seleccione una opción (1-5): ");
+
+            opcion = scanner.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    System.out.println("Ha seleccionado la Comprar vuelo");
+                    // Aquí puedes poner el código que deseas ejecutar para la Comprar vuelo.
+                    break;
+                case 2:
+                    System.out.println("Ha seleccionado la Reasignar vuelo");
+                    // Aquí puedes poner el código que deseas ejecutar para la Reasignar vuelo.
+                    break;
+                case 3:
+                    System.out.println("Ha seleccionado la Cancelar vuelo");
+                    // Aquí puedes poner el código que deseas ejecutar para la Cancelar vuelo.
+                    break;
+                case 4:
+                    System.out.println("Ha seleccionado la Ver cuenta");
+                    // Aquí puedes poner el código que deseas ejecutar para la Ver cuenta.
+                    break;
+                case 5:
+                    System.out.println("Saliendo del programa. ¡Adiós!");
+                    break;
+                default:
+                    System.out.println("Opción no válida. Por favor, seleccione una opción válida (1-5).");
+                    break;
+            }
+        } while (opcion != 5);
+
+        scanner.close();
+
+
+
     }
+
 }
