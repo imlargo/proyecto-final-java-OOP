593788064a71915b24996becfdca4dc10a561ca7
AlejaMuGo <141966575+AlejaMuGo@users.noreply.github.com>
2023-10-24 17:31:14 +0000

    docs

--- src/gestorAplicacion/Cuenta/GestionUsuario.java
+++ src/gestorAplicacion/Cuenta/GestionUsuario.java
@@ -2,10 +2,13 @@ package gestorAplicacion.Cuenta;
 
 import java.io.Serializable;
 import java.util.ArrayList;
-
-import static uiMain.Estetica.*;
 import gestorAplicacion.Aerolinea.*;
+import static uiMain.Estetica.*;
 
+/**
+ * La clase GestionUsuario se encarga de gestionar a los usuarios, incluyendo iniciar sesión,
+ * cambiar contraseñas y llevar un inventario de maletas.
+ */
 public class GestionUsuario implements Serializable {
 
     private static final long serialVersionUID = 1L;
@@ -14,6 +17,9 @@ public class GestionUsuario implements Serializable {
     public Usuario user = null;
     public ArrayList<Maleta> inventarioMaletas = new ArrayList<>();
 
+    /**
+     * Constructor de la clase GestionUsuario que inicializa un usuario de ejemplo y lo agrega a la lista de usuarios.
+     */
     public GestionUsuario() {
         Usuario user = new Usuario("Jaime A. Guzman", "usuario@gmail.com", "123", 0);
         user.setDinero(2000);
@@ -21,6 +27,13 @@ public class GestionUsuario implements Serializable {
         Usuarios.add(user);
     }
 
+    /**
+     * Inicia sesión de un usuario utilizando su dirección de correo electrónico y contraseña.
+     *
+     * @param mail       La dirección de correo electrónico del usuario.
+     * @param contrasena La contraseña del usuario.
+     * @return El usuario que ha iniciado sesión o null si no se encuentra.
+     */
     public Usuario iniciarSesion(String mail, String contrasena) {
         for (Usuario usuario : Usuarios) {
             if (usuario.getMail().equals(mail) && usuario.verificarContrasena(contrasena)) {
@@ -31,8 +44,15 @@ public class GestionUsuario implements Serializable {
         return null;
     }
 
+    /**
+     * Cambia la contraseña de un usuario si la contraseña actual es correcta.
+     *
+     * @param usuario         El usuario que desea cambiar su contraseña.
+     * @param contrasena      La contraseña actual del usuario.
+     * @param nuevaContrasena La nueva contraseña que se desea establecer.
+     * @return El usuario con su contraseña cambiada o null si la contraseña actual es incorrecta.
+     */
     public Usuario cambiarContrasena(Usuario usuario, String contrasena, String nuevaContrasena) {
-
         if (usuario.verificarContrasena(contrasena)) {
             usuario.setContrasena(nuevaContrasena);
             return usuario;
@@ -41,13 +61,23 @@ public class GestionUsuario implements Serializable {
         }
     }
 
+    /**
+     * Obtiene el usuario actualmente conectado.
+     *
+     * @return El usuario actualmente conectado o null si no hay una sesión activa.
+     */
     public Usuario getUser() {
         return this.user;
     }
 
+    /**
+     * Cierra la sesión del usuario actual.
+     *
+     * @param user El usuario cuya sesión se va a cerrar.
+     * @return Null para indicar que no hay un usuario en sesión.
+     */
     public Usuario cerrarSesion(Usuario user) {
         this.user = null;
         return null;
     }
-
 }
