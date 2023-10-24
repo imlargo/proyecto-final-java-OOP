240af65350539ec05d2a8386f12517be4dc8766f
AlejaMuGo <141966575+AlejaMuGo@users.noreply.github.com>
2023-10-24 09:56:37 +0000

    fix

--- src/gestorAplicacion/Descuentos/descuentoVuelo.java
+++ src/gestorAplicacion/Descuentos/descuentoVuelo.java
@@ -27,32 +27,3 @@ public class descuentoVuelo extends Descuento implements Serializable {
     }
 
 }
-package gestorAplicacion.Descuentos;
-
-import java.io.Serializable;
-import gestorAplicacion.Cuenta.*;
-import gestorAplicacion.Aerolinea.*;
-
-import static uiMain.Estetica.*;
-
-public class descuentoVuelo extends Descuento implements Serializable {
-    private static final long serialVersionUID = 1L;
-
-    public static int costoMillas = 1;
-    public static int descuento = 20;
-
-    public descuentoVuelo(Usuario user) {
-        this.init(user);
-        this.tipo = "Descuento Vuelo";
-    }
-
-    public void aplicarDescuento(Boleto boleto) {
-        this.boleto = boleto;
-        double retorno = descuento / 100;
-        float valorVuelo = boleto.getValorEquipaje();
-        boleto.setValorInicial((float) (valorVuelo * (1 - retorno)));
-        this.user.depositarDinero((int) (valorVuelo * (retorno)));
-        this.usar();
-    }
-
-}
